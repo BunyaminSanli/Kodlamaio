07429e09471daf60c2935ed27b9a99f8692dac3d
BunyaminSanli <60830225+BunyaminSanli@users.noreply.github.com>
2021-03-08 23:24:25 +0300

    Proje dosyası ekle.

---
+++ Kodlamaio/Musteri.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kodlamaio
+{
+    class Musteri
+    {
+        
+        string ad;
+        public string GetAd()
+        {
+            return ad;
+        }
+
+        public void SetAd(string ad)
+        {
+            this.ad = ad;
+        }
+
+        string soyad;
+        public string GetSoyad()
+        {
+            return soyad;
+        }
+
+        public void SetSoyad(string soyad)
+        {
+            this.soyad = soyad;
+        }
+
+        string adres;
+        public string GetAdres()
+        {
+            return adres;
+        }
+
+        public void SetAdres(string adres)
+        {
+            this.adres = adres;
+        }
+
+        char cinsiyet;
+        public char GetCinsiyet()
+        {
+            return cinsiyet;
+        }
+
+        public void SetCinsiyet(char cinsiyet)
+        {
+            this.cinsiyet = cinsiyet;
+        }
+
+
+        int id;
+        public int GetId()
+        {
+            return id;
+        }
+
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+    }
+}
---
+++ Kodlamaio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kodlamaio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.SetId(5);
+            Console.WriteLine(musteri.GetId());
+        }
+    }
+}
